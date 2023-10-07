21b3d4ca10ada10156f77d9f43ff6c7b1b5a4b7e
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-10-07 10:16:52 +0530

    Made the grid

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+GREY = (80, 80, 80)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
+
+
+def draw_grid(positions):
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen, GREY, (0, row * TILE_SIZE),
+                         (WIDTH, row * TILE_SIZE))
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen, GREY, (col * TILE_SIZE, 0),
+                         (col * TILE_SIZE, HEIGHT))
+
+
+def main():
+    running = True
+    positions = set()
+    while (running):
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(BLACK)
+        draw_grid(positions)
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
